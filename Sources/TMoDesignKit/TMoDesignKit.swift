aa1a9e9d73fa06046fe0705ff5b46c0b442da33a
AmithShaju1 <amithshaju@gmail.com>
2022-07-26 12:59:03 +0530

    added Credit class

--- Sources/TMoDesignKit/TMoDesignKit.swift
+++ Sources/TMoDesignKit/TMoDesignKit.swift
@@ -141,4 +141,42 @@ public struct TMoDesignKit {
 //        }
     }
     
+    public class Credit {
+        enum Status : String {
+            case new = "New"
+            case processing = "Processing"
+            case approved = "Active"
+            case closed = "Closed"
+        }
+        public var status : String?
+        public var monthlyBillCredit : Int?
+        public var creditBalance : Int?
+        public var startDate : Date?
+        public var duration : String?
+        public var promoBalance : PromoBalance
+        
+        public init() {
+            status = Status.new.rawValue
+            monthlyBillCredit = 0
+            creditBalance = 0
+            startDate = Date()
+            duration = "Not Decided"
+            promoBalance = PromoBalance(initialCreditBalance: 0, remainingCredit: 0, totalCreditAmount: 0)
+            
+        }
+    }
+
+    public struct PromoBalance {
+        public var initialCreditBalance : Int?
+        public var remainingCredit : Int?
+        public var totalCreditAmount : Int?
+        
+        public init(initialCreditBalance: Int, remainingCredit: Int, totalCreditAmount: Int) {
+            self.initialCreditBalance = initialCreditBalance
+            self.remainingCredit = remainingCredit
+            self.totalCreditAmount = totalCreditAmount
+        }
+    }
+
+    
 }
